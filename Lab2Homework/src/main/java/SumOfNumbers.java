09391b2ccf973087fb82fcf9ed586248d9ab5687
chase <chase@LAPTOP-3HQQBMAM>
2019-09-16 16:49:57 -0500

    lab 2 part 3

---
+++ Lab2Homework/src/main/java/SumOfNumbers.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chase
+ */
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        
+        //Ask user for input
+        Scanner keyboard = new Scanner(System.in);
+        
+        int userNumber = 0;
+        int sum = 0;
+        int i;
+        
+        //Retrieve user number
+        System.out.println("Please enter a non-zero, positive integer: ");
+        userNumber = keyboard.nextInt();
+       
+        
+        //Start the loop to calculate the sum
+        for (i = 1; i <= userNumber; i++) {
+            sum = sum + i;
+        }
+        
+        //Display the sum
+        System.out.println("The sum of the number is equal to: " + sum);
+        
+    }
+}
