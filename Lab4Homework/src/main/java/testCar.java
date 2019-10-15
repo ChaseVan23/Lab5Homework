bdcaf8e40d11850f7bbca0d6ba6b9c86ad8c6abc
chase <chase@LAPTOP-3HQQBMAM>
2019-10-14 19:42:38 -0500

    lab4

--- Lab4Homework/src/main/java/testCar.java
+++ Lab4Homework/src/main/java/testCar.java
@@ -6,7 +6,7 @@ public class testCar {
     public static void main(String[] args) throws IOException{
 
         Car c = new Car(1996, "Audi");
-   
+    
         System.out.println("Starting Car Speed : " + c.getCarSpeed());
 
         for(int i=0;i<5;i++)
